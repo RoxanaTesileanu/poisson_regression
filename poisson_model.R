cc2df6380bf8eb5bd8e20530c317522958997d2a
Roxana Tesileanu <roxana.te@web.de>
2017-06-08 11:12:27 +0300

    adding plots to the analysis

--- poisson_model.R
+++ poisson_model.R
@@ -6,7 +6,7 @@
 mydata <- read.csv("/home/roxana/poisson_data.csv", header=TRUE)
 attach(mydata)
 names(mydata)
-
+summary(mydata)
 plot(panta, vizuini)
 model1 <- glm(vizuini ~ panta , data=mydata, family=poisson()) # Poisson as a pmf with one parameter: the mean (dispersion parameter set to 1)
 summary(model1)
@@ -16,6 +16,14 @@ model2 <- glm(vizuini ~panta, data = mydata, family= quasipoisson())
 # the dispersion parameter is estimated from the data and not set to 1
 
 summary(model2)
+plot(model2)
+
+plot(predict(model2, type="response"), residuals(model2, type="deviance"))
+plot(hatvalues(model2))
+plot(rstudent(model2))
+plot(cooks.distance(model2))
+
+
 
 library(robust)
 model3 <- glmRob(vizuini~panta, family = poisson(), data= mydata)
@@ -25,6 +33,29 @@ summary(model3)
 
 # 2) Plotting the results
 
+#plot 1
+pathToPlot <- "/home/roxana/poisson_regression/plot1.bmp"
+bmp(pathToPlot, width = 500, height = 500)
+plot(panta, vizuini, main= "Distribution of beaver dam counts along the slope range", ylab= "nr. vizuini", xlab= "panta")
+dev.off()
 
+#plot 2 
+newXs <- seq(0,22,0.1)
+newXs
+coef(model2)
+newLogMu <- 4.6587012 -0.2314341*newXs
+newLogMu
+newdf <- data.frame(newXs, newLogMu)
+plot(newdf)
+
+newdf2 <- data.frame(newXs, exp(newLogMu))
+plot(newdf2)
+points(mydata)
 
 # 3) Conclusions
+
+# 4) References: 
+# R I Kabacoff, 2015: R in action - Data analysis and graphics with R,  second edition, Manning
+# M J Crawley, 2013: The R book, second edition, Wiley 
+# G P Quinn, M J Keough, 2002: Experimental design and data analysis for biologists, CUP
+
